e9ae2fc652c41fe152790ebef4322de055cad85f
Jan-Oliver Jahner <jan-oliver.jahner@tolingo.com>
2011-10-07 00:27:33 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '412f0017b597742e16632a571c7961dd92cab17c1b76574127adc7f3277e6eace1b21fbe08707cccaafaa215432989493b942d8cf027e0b9e9358a115b014a16'
