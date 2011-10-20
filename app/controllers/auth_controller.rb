ec1cffdfc1ea679d5db983cd1a6dc7945ae6439f
Jan-Oliver Jahner <jan-oliver.jahner@tolingo.com>
2011-10-20 15:38:58 +0200

    Improve auto controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,22 +1,28 @@
 class AuthController < ApplicationController
   before_filter :require_login, :except => [:access_token]
   skip_before_filter :verify_authenticity_token, :only => [:access_token]
-  
+
   def authorize
-    redirect_to params[:redirect_uri]
+    application = Application.where(key: params[:client_id])
+    if application
+      redirect_to add_params_to_uri(params[:redirect_uri], { error: type })
+    else
+      state = {state: params[:state]} if params[:state]
+      redirect_to add_params_to_uri(params[:redirect_uri], { error: type })
+    end
   end
-  
+
   def access_token
     hash = {
       # token generieren und speichern (user, application) => tokens
-      
-      :access_token  => "#{SecureRandom.hex(16)}", 
-      :refresh_token => "#{SecureRandom.hex(16)}", 
+
+      :access_token  => "#{SecureRandom.hex(16)}",
+      :refresh_token => "#{SecureRandom.hex(16)}",
       :expires_in    => 10
     }
     render :json => hash.to_json
   end
-  
+
   def user
     hash = {
       :provider => 'homi_id',
@@ -27,5 +33,13 @@ class AuthController < ApplicationController
     }
 
     render :json => hash.to_json
+  end
+
+  private
+
+  def add_params_to_uri(uri, params)
+    uri = URI.parse(uri)
+    uri.query = [uri.query, params.to_query].compact.join('&')
+    uri.to_s
   end
 end
--- features/home.feature
+++ features/home.feature
@@ -9,8 +9,8 @@ Feature: Visit Home
       | testuser | user@email.com | secret   |
     Given I am a guest
     When  I go to root page
-     And  I fill in 'user_login' with 'testuser'
-     And  I fill in 'user_password' with 'secret'
+     And  I fill in 'login' with 'testuser'
+     And  I fill in 'password' with 'secret'
      And  I press 'Login'
     Then  I should be on my profile page
 
@@ -20,7 +20,7 @@ Feature: Visit Home
       | testuser | user@email.com | secret   |
     Given I am a guest
     When  I go to root page
-     And  I fill in 'user_login' with 'user@email.com'
-     And  I fill in 'user_password' with 'secret'
+     And  I fill in 'login' with 'user@email.com'
+     And  I fill in 'password' with 'secret'
      And  I press 'Login'
     Then  I should be on my profile page
